0a44708517388f0880c867a779eb89d5c39e8df0
santiagobonilla14 <111590968+santiagobonilla14@users.noreply.github.com>
2022-10-12 16:31:56 -0500

    Update schema.py

--- app/schema.py
+++ app/schema.py
@@ -1,12 +1,23 @@
 instructions = [
-    'DROP TABLE IF EXISTS usuario;',
+  instructions = [
+    'DROP TABLE IF EXISTS hecho;',
+    """
+        CREATE TABLE hecho (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            hecho  VARCHAR(100) NOT NULL,
+            estado VARCHAR(45) NOT NULL
+             );
+
+        """
+        'DROP TABLE IF EXISTS usuario;',
     """
         CREATE TABLE usuario (
             id INT PRIMARY KEY AUTO_INCREMENT,
             user  VARCHAR(50) NOT NULL,
-            password VARCHAR(50) NOT NULL
+            password VARCHAR(102) NOT NULL
         )
         """
+    
 ]
     
  
