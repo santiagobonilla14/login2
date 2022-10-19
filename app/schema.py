86b212dd7cc3419d7b44a908e24faccb822d27b6
santiagobonilla14 <111590968+santiagobonilla14@users.noreply.github.com>
2022-10-18 23:05:21 -0500

    Update schema.py

--- app/schema.py
+++ app/schema.py
@@ -1,19 +1,27 @@
 instructions = [
-    'DROP TABLE IF EXISTS hecho;',
-    """
-        CREATE TABLE hecho (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            hecho  VARCHAR(100) NOT NULL,
-            estado VARCHAR(45) NOT NULL
-            )
-""",
+    'SET FOREIGN_KEY_CHECKS=0;',
+    'DROP TABLE IF EXISTS hechos;',
+    'DROP TABLE IF EXISTS usuario;',
+    'SET FOREIGN_KEY_CHECKS=1;',
 
-'DROP TABLE IF EXISTS usuario;',
-"""
+    """
         CREATE TABLE usuario (
             id INT PRIMARY KEY AUTO_INCREMENT,
-            user  VARCHAR(50) NOT NULL,
+            username  VARCHAR(50) NOT NULL,
             password VARCHAR(102) NOT NULL
         )
     """
+    ,
+    """
+        CREATE TABLE hechos (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            hecho  VARCHAR(100) NOT NULL,
+            estado VARCHAR(45) NOT NULL,
+            created_by INT NOT NULL,
+            created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            completed BOOLEAN NOT NULL,
+            FOREIGN KEY (created_by) REFERENCES usuario (id)
+           
+            )
+"""
 ]
